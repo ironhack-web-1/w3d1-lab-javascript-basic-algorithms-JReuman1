15c5881537af48f312dfa6fd6e83917edba41486
JReuman1 <99495156+JReuman1@users.noreply.github.com>
2022-07-19 19:50:01 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
 // Iteration 1: Names and Input
-//
+
+let hacker1 = "Javier";
+
+console.log("The driver's name is " + hacker1)
+
+let hacker2 = "Pedro";
+
+console.log("The navigator's name is " + hacker2)
+
+
 // Iteration 2: Conditionals
 
 
