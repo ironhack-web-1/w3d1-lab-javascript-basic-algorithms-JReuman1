8d336b91f734e46c0ee9c5f67c4c9a902872f87f
JReuman1 <99495156+JReuman1@users.noreply.github.com>
2022-07-23 09:51:10 +0200

    Agregar respuestas de las iteraciones. Queda pendiente realizar los bonus

--- js/index.js
+++ js/index.js
@@ -8,8 +8,55 @@ let hacker2 = "Pedro";
 
 console.log("The navigator's name is " + hacker2)
 
-
 // Iteration 2: Conditionals
 
+if (hacker1.length > hacker2.length) {
+    console.log("The driver has the longest name, it has " + hacker1.length + " characters.")
+}
+
+else if (hacker1.length < hacker2.length) {
+    console.log("It seems that the navigator has the longest name, it has " + hacker2.length + " characters.")
+}
+
+else {
+    console.log("Wow, you both have equally long names, " + hacker1.length + " characters!")
+}
 
 // Iteration 3: Loops
+//3.1
+
+let ans = "";
+
+for (let i = 0; i <= hacker1.length - 1; i++){
+    
+  ans += " " + (hacker1[i].toUpperCase())
+}
+
+console.log(ans);
+
+//3.2
+
+let ans1 = "";
+
+for (let i = hacker2.length - 1; i >= 0; i--){
+  ans1 += " " + hacker2[i]
+ }
+ 
+ console.log(ans1);
+
+ //3.3
+ 
+ while (hacker1 < hacker2){
+   console.log("The driver's name goes first.");
+   break;
+ }
+ 
+ while (hacker1 > hacker2){
+   console.log("Yo, the navigator goes first definitely")
+   break;
+ }
+ 
+ while (hacker1 == hacker2){
+   console.log("What?! You both have the same name?")
+   break;
+ }
